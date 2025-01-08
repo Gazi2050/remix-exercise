3e63d3fccfd6398e78ab7b822c7e2f072f4c100d
Gazi2050 <gazi45344@gmail.com>
2025-01-08 21:47:30 +0600

    first commit

---
+++ 01-routing/app/routes/posts/index.tsx
@@ -0,0 +1,10 @@
+
+const index = () => {
+    return (
+        <main>
+            <h1 className="text-center text-2xl font-bold mt-10">Posts</h1>
+        </main>
+    );
+};
+
+export default index;
